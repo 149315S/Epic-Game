b842c0d017e20b49358730be0330de5f4eb1bf32
149315S <149315@student.vansd.org>
2022-10-27 09:12:14 -0700

    asd
    asd

---
+++ Coding With Carl Lesson 3/Assets/Scripts/MovementOfWorld.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementOfWorld : MonoBehaviour
+{
+    private float speed = 15;
+    private PlayerController playercontrollerscript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerControllerScript.gameOver == false)
+        {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+    }
+}
--- Coding With Carl Lesson 3/Assets/Scripts/PlayerController.cs
+++ Coding With Carl Lesson 3/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce = 10;
     public float gravityModifier = 1;
+    public bool isOnGround = true;
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +19,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        { 
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
     }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.CompareTag("Ground"))
+            {
+                isOnGround = true;
+            }
+            else if(collision.gameObject.CompareTag("Obstacle"))
+            {
+                Debug.Log("Game Over");
+                gameOver = true;
+            }
+        }
 }
